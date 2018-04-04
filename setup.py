8146deee5b72312feaca960d87a52a72ce6d3ad6
Anguelos <anguelos.nicolaou@gmail.com>
2018-04-04 20:49:45 +0200

    Added README.md

--- setup.py
+++ setup.py
@@ -8,5 +8,8 @@ setup(
     packages=['dagtasets',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+    install_requires=[
+        'torch','torchvision','numpy'
+    ],
 )
 
