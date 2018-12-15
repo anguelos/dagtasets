e4f76db15ba8774450bf4e729fd6a5544cffe543
Anguelos <anguelos.nicolaou@gmail.com>
2018-12-15 23:32:21 +0100

    cleaned up lm_util.argv from debug prints

--- lm_util/argv.py
+++ lm_util/argv.py
@@ -2,6 +2,19 @@ import sys
 import re
 
 def get_arg_switches(default_switches, argv=None):
+    new_default_switches={}
+    switches_help = {"help":"Print help and exit."}
+    for k,v in default_switches.items():
+        if  hasattr(v, '__len__') and len(v)==2 and isinstance(v[1], basestring):
+            switches_help[k]=v[1]
+            new_default_switches[k]=v[0]
+        else:
+            switches_help[k] = ""
+            new_default_switches[k] = v
+    default_switches=new_default_switches
+    del new_default_switches
+
+
     default_switches = dict(default_switches, **{"help": False})
     if argv is None:
         argv = sys.argv
@@ -10,8 +23,8 @@ def get_arg_switches(default_switches, argv=None):
     if set(argv_switches.keys()) > set(default_switches.keys()):
         help_str = "\n" + argv[0] + " Syntax:\n\n"
         for k in default_switches.keys():
-            help_str += "\t-%s=%s. Default %s\n" % (
-                k, repr(type(default_switches[k])), repr(default_switches[k]))
+            help_str += "\t-%s=%s %s Default %s.\n" % (
+                k, repr(type(default_switches[k])), switches_help[k], repr(default_switches[k]))
         help_str += "\n\nUrecognized switches: "+repr(tuple( set(default_switches.keys()) - set(argv_switches.keys())))
         help_str += "\nAborting.\n"
         sys.stderr.write(help_str)
@@ -26,13 +39,13 @@ def get_arg_switches(default_switches, argv=None):
     help_str = "\n" + argv[0] + " Syntax:\n\n"
 
     for k in default_switches.keys():
-        help_str += "\t-%s=%s. Default %s . Passed %s\n" % (
-        k, repr(type(default_switches[k])), repr(default_switches[k]), repr(argv_switches[k]))
+        help_str += "\t-%s=%s %s Default %s . Passed %s\n" % (
+        k, repr(type(default_switches[k])), switches_help[k], repr(default_switches[k]), repr(argv_switches[k]))
     help_str += "\nAborting.\n"
 
     #replace {blabla} with argv_switches["balbla"] values
     replacable_values=["{"+k+"}" for k in argv_switches.keys()]
-    while len(re.findall("{[a-z0-9A-Z_]+}",repr(argv_switches.values()))):
+    while len(re.findall("{[a-z0-9A-Z_]+}","".join([v for v in argv_switches.values() if isinstance(v,str)]))):
         for k,v in argv_switches.items():
             if isinstance(v,str):
                 argv_switches[k]=v.format(**argv_switches)
