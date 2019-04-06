34ae24f4125068f4fd1576cd10a84bf789d4ba22
Anguelos <anguelos.nicolaou@gmail.com>
2019-04-06 16:29:59 +0200

    added __init__.py

--- lm_util/__init__.py
+++ lm_util/__init__.py
@@ -1,4 +1,4 @@
 from .argv import get_arg_switches
-from .colored_strings import rgbstr, render_sequence
+from .colored_strings import rgbstr, render_sequence, render_prediction
 from .encoder import Encoder, alphanumeric_encoder, letter_encoder
 from .corpora import OcrCorpus
